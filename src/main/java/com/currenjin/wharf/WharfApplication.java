3db50076b9b091993aa8c5a98ef4551cc443a0c9
currenjin <hyun0524e@naver.com>
2024-12-03 17:56:19 +0900

    Feat: Init project

---
+++ src/main/java/com/currenjin/wharf/WharfApplication.java
@@ -0,0 +1,13 @@
+package com.currenjin.wharf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WharfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WharfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/currenjin/wharf/WharfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currenjin.wharf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WharfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
