6747dcbf5c0186c682d63458d8d132e05b2af36e
currenjin <hyun0524e@naver.com>
2024-12-04 15:01:09 +0900

    Refactor: Detect file string to constant

--- src/main/java/com/currenjin/wharf/detector/NodeFrameworkDetector.java
+++ src/main/java/com/currenjin/wharf/detector/NodeFrameworkDetector.java
@@ -6,9 +6,11 @@ import java.nio.file.Path;
 import com.currenjin.wharf.domain.Framework;
 
 public class NodeFrameworkDetector implements FrameworkDetector {
+	public static final String PACKAGE_JSON = "package.json";
+
 	@Override
 	public Framework detect(Path path) {
-		if (Files.exists(path.resolve("package.json"))) {
+		if (Files.exists(path.resolve(PACKAGE_JSON))) {
 			return Framework.NODE_JS;
 		}
 		return Framework.UNKNOWN;
--- src/main/java/com/currenjin/wharf/detector/SpringBootFrameworkDetector.java
+++ src/main/java/com/currenjin/wharf/detector/SpringBootFrameworkDetector.java
@@ -7,9 +7,12 @@ import java.nio.file.Path;
 import com.currenjin.wharf.domain.Framework;
 
 public class SpringBootFrameworkDetector implements FrameworkDetector {
+	public static final String BUILD_GRADLE = "build.gradle";
+	public static final String ORG_SPRINGFRAMEWORK_BOOT = "org.springframework.boot";
+
 	@Override
 	public Framework detect(Path path) {
-		Path buildGradlePath = path.resolve("build.gradle");
+		Path buildGradlePath = path.resolve(BUILD_GRADLE);
 
 		if (!Files.exists(buildGradlePath)) {
 			return Framework.UNKNOWN;
@@ -17,7 +20,7 @@ public class SpringBootFrameworkDetector implements FrameworkDetector {
 
 		try {
 			String buildGradle = Files.readString(buildGradlePath);
-			if (buildGradle.contains("org.springframework.boot")) {
+			if (buildGradle.contains(ORG_SPRINGFRAMEWORK_BOOT)) {
 				return Framework.SPRING_BOOT;
 			}
 			return Framework.UNKNOWN;
