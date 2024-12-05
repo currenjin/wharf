7b05cdd2bc253c526319d463f96dcf33a637dd26
currenjin <hyun0524e@naver.com>
2024-12-05 14:55:35 +0900

    Test: Coverage for FrameworkDetector(IOException)

--- src/test/java/com/currenjin/wharf/detector/SpringBootFrameworkDetectorTest.java
+++ src/test/java/com/currenjin/wharf/detector/SpringBootFrameworkDetectorTest.java
@@ -2,9 +2,13 @@ package com.currenjin.wharf.detector;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import com.currenjin.wharf.domain.Framework;
 
@@ -34,6 +38,17 @@ public class SpringBootFrameworkDetectorTest {
 		Path path = Path.of("src/test/resources/empty-project");
 
 		Framework detected = detector.detect(path);
+
+		assertThat(detected).isEqualTo(Framework.UNKNOWN);
+	}
+
+	@Test
+	void detectUnknownWhenIOException(@TempDir Path tempDir) throws IOException {
+		Path buildGradlePath = tempDir.resolve("build.gradle");
+		Files.writeString(buildGradlePath, "content");
+		Files.setPosixFilePermissions(buildGradlePath, Collections.emptySet());
+
+		Framework detected = detector.detect(tempDir);
 
 		assertThat(detected).isEqualTo(Framework.UNKNOWN);
 	}
