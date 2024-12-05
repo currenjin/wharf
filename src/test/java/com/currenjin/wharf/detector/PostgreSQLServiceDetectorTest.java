683a249a7db3c78c1b68672a28d89cdc1f55b160
currenjin <hyun0524e@naver.com>
2024-12-05 15:10:12 +0900

    Test: Coverage for PostgreSQLServiceDetector

---
+++ src/test/java/com/currenjin/wharf/detector/PostgreSQLServiceDetectorTest.java
@@ -0,0 +1,73 @@
+package com.currenjin.wharf.detector;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import com.currenjin.wharf.domain.Service;
+import com.currenjin.wharf.domain.ServiceType;
+
+class PostgreSQLServiceDetectorTest {
+	@Test
+	void detectPostgreSQLFromGradle(@TempDir Path tempDir) throws Exception {
+		createGradleWithPostgreSQLDependency(tempDir);
+		ServiceDetector detector = new PostgreSQLServiceDetector();
+
+		List<Service> services = detector.detect(tempDir);
+
+		assertThat(services).hasSize(1);
+		assertThat(services.get(0))
+			.satisfies(postgresql -> {
+				assertThat(postgresql.getName()).isEqualTo("postgresql");
+				assertThat(postgresql.getVersion()).isEqualTo("15.0");
+				assertThat(postgresql.getType()).isEqualTo(ServiceType.DATABASE);
+			});
+	}
+
+
+	@Test
+	void returnEmptyListWhenBuildGradleIsEmpty(@TempDir Path tempDir) throws Exception {
+		Files.writeString(tempDir.resolve("build.gradle"), "");
+		ServiceDetector detector = new PostgreSQLServiceDetector();
+
+		List<Service> services = detector.detect(tempDir);
+
+		assertThat(services).isEmpty();
+	}
+
+	@Test
+	void returnEmptyListWhenNoBuildGradle(@TempDir Path tempDir) {
+		ServiceDetector detector = new PostgreSQLServiceDetector();
+
+		List<Service> services = detector.detect(tempDir);
+
+		assertThat(services).isEmpty();
+	}
+
+	@Test
+	void returnEmptyListWhenIOException(@TempDir Path tempDir) throws Exception {
+		Path buildGradlePath = tempDir.resolve("build.gradle");
+		Files.writeString(buildGradlePath, "content");
+		Files.setPosixFilePermissions(buildGradlePath, Collections.emptySet());
+		ServiceDetector detector = new PostgreSQLServiceDetector();
+
+		List<Service> services = detector.detect(tempDir);
+
+		assertThat(services).isEmpty();
+	}
+
+	private void createGradleWithPostgreSQLDependency(Path directory) throws Exception {
+		String buildGradle = """
+                dependencies {
+                    implementation 'org.postgresql:postgresql:42.6.0'
+                }
+                """;
+		Files.writeString(directory.resolve("build.gradle"), buildGradle);
+	}
+}
