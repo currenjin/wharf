358ea27386ab3078c3a4925e7acc1003d00fbb5a
currenjin <hyun0524e@naver.com>
2024-12-11 23:27:57 +0900

    Improve: ServiceDetector return to single service

--- src/main/java/com/currenjin/wharf/detector/ServiceDetector.java
+++ src/main/java/com/currenjin/wharf/detector/ServiceDetector.java
@@ -3,8 +3,7 @@ package com.currenjin.wharf.detector;
 import com.currenjin.wharf.domain.Service;
 
 import java.nio.file.Path;
-import java.util.List;
 
 public interface ServiceDetector {
-    List<Service> detect(Path path);
+    Service detect(Path path);
 }
