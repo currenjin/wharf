500036ecbb7db512f009307e004156bbb873f4dc
currenjin <hyun0524e@naver.com>
2024-12-14 19:04:58 +0900

    Test: ConfigurationLoadException

--- src/test/java/com/currenjin/wharf/loader/ServiceConfigLoaderTest.java
+++ src/test/java/com/currenjin/wharf/loader/ServiceConfigLoaderTest.java
@@ -3,7 +3,10 @@ package com.currenjin.wharf.loader;
 import com.currenjin.wharf.domain.ServiceType;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ServiceConfigLoaderTest {
     @Test
@@ -19,5 +22,12 @@ class ServiceConfigLoaderTest {
 
         assertEquals(actual.services().get(1).name(), "postgresql");
         assertEquals(actual.services().get(1).type(), ServiceType.DATABASE);
+    }
+
+    @Test
+    void loadNullTest() {
+        String path = "invalid_services.yml";
+
+        assertThrows(ConfigurationLoadException.class, () -> ServiceConfigLoader.load(path));
     }
 }
